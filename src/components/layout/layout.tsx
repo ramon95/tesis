30f974269f6546fd2d18391c7d1361921dee192d
Ramon Perez <ramon@rebustech.io>
2024-03-28 16:12:29 -0500

    feat: add new components and configuration files

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React from "react"
+import { Navbar } from ".."
+
+export const Layout = () => {
+  return (
+    <div className="bg-white">
+      <Navbar />
+    </div>
+  )
+}
